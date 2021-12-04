d66be311fd451ece12a474aa2e27b854a99498c7
mp4 <puleri.mp4@gmail.com>
2021-12-04 10:53:00 -0800

    chat room functional

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,33 @@
-console.log("hello world");
+import express from 'express';
+import {createServer} from 'http'
+import {Server} from 'socket.io'
+import cors from "cors"
+import config from "config"
+import logger from './utils/logger'
+
+import socket from './socket'
+
+const port = config.get<number>("port");
+const host = config.get<string>('host');
+const corsOrigin = config.get<string>('corsOrigin')
+
+
+const app = express()
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials: true,
+  }
+})
+
+app.get('/', (_, res) => res.send('Server is up'))
+
+httpServer.listen(port, host, () => {
+  logger.info('🚀 Server is listening 🚀')
+  logger.info(`http://${host}:${port}`)
+
+  socket({ io });
+})
